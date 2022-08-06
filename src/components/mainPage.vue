8db6e312c06e25737318f978220437c63ee0ba98
朱永博 <995231030@qq.com>
2022-08-06 18:50:48 +0800

    noraml

---
+++ src/components/mainPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        哈哈哈
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+</style>
